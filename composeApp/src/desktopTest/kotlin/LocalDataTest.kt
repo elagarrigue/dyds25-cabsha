26058c55c4a95fc88d166e5c8c0a036afa7cf3d9
Agustin Vera <agus22v@outlook.com>
2025-06-18 12:01:41 -0300

    Implementacion del test LocalDataTest

---
+++ composeApp/src/desktopTest/kotlin/LocalDataTest.kt
@@ -0,0 +1,76 @@
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import edu.dyds.movies.data.local.LocalDataCache
+import edu.dyds.movies.data.local.LocalDataSource
+import edu.dyds.movies.domain.entity.Movie
+import org.junit.Test
+
+class LocalDataCacheTest {
+
+    @Test
+    fun `getMovies deberia retornar la lista vacia cuando inicia`() {
+        // Arrange
+        val localCacheMovies: LocalDataSource = LocalDataCache()
+
+        // Act
+        val result = localCacheMovies.getMovies()
+
+        // Assert
+        assertTrue(result.isEmpty())
+    }
+
+    @Test
+    fun `setMovies deberia almacenar y retornar los valores correctamente`() {
+        // Arrange
+        val localCacheMovies: LocalDataSource = LocalDataCache()
+        val movies = listOf(
+            Movie(1, "Película A", "Overview A", "2023-01-01", "posterA", null, "Titulo Original A", "en", 10.0, 8.5),
+            Movie(2, "Película B", "Overview B", "2023-01-01", "posterB", null, "Titulo Original B", "en", 10.0, 7.0)
+        )
+
+        // Act
+        localCacheMovies.setMovies(movies)
+        val result = localCacheMovies.getMovies()
+
+        // Assert
+        assertEquals(movies, result)
+    }
+
+    @Test
+    fun `setMovies deberia reemplazar completamente la lista existente`() {
+        // Arrange
+        val localCacheMovies: LocalDataSource = LocalDataCache()
+        val initialListMovies = listOf(
+            Movie(1, "Película Inicial", "Overview", "2023-01-01", "poster", null, "Original", "en", 10.0, 8.5)
+        )
+        val newListMovies = listOf(
+            Movie(2, "Nueva Película", "Overview", "2023-01-01", "poster", null, "Original", "en", 10.0, 7.0)
+        )
+
+        // Act
+        localCacheMovies.setMovies(initialListMovies)
+        localCacheMovies.setMovies(newListMovies)
+        val result = localCacheMovies.getMovies()
+
+        // Assert
+        assertEquals(newListMovies, result) // Verifica que la lista inicial fue reemplazada
+        assertEquals(1, result.size) // Asegura que no hay elementos previos
+    }
+
+    @Test
+    fun `setMovies deberia vaciar la lista cuando recibe una lista vacia`() {
+        // Arrange
+        val localCacheMovies: LocalDataSource = LocalDataCache()
+        val initialListMovies = listOf(
+            Movie(1, "Película Inicial", "Overview", "2023-01-01", "poster", null, "Original", "en", 10.0, 8.5)
+        )
+
+        // Acts
+        localCacheMovies.setMovies(initialListMovies)
+        localCacheMovies.setMovies(emptyList())
+        val result = localCacheMovies.getMovies()
+
+        // Assert
+        assertTrue(result.isEmpty()) // Verifica que la caché queda vacía
+    }
+}
